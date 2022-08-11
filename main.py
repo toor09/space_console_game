72e74fc9f7fc2911e12fcbb7e498d68d55beb1af
Danil Nikitin <d.nikitin.dev@gmail.com>
2022-08-11 15:19:43 +0300

    Added an infinite twinkling star.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import curses
 import time
+from collections import namedtuple
 
 
 def draw(canvas):  # type: ignore
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
     canvas.border()
-    canvas.refresh()
-    time.sleep(1)
+    curses.curs_set(False)
+    star = "*"
+    StarFrame = namedtuple("StarFrame", "delay style")
+    dim_star_frame = StarFrame(delay=2, style=curses.A_DIM)
+    bold_star_frame = StarFrame(delay=0.3, style=curses.A_BOLD)
+    default_star_frame = StarFrame(delay=0.3, style=0)
+    frames = (
+        dim_star_frame,
+        default_star_frame,
+        bold_star_frame,
+        default_star_frame,
+    )
+    for delay_frame, style_frame in frames:
+        canvas.addstr(row, column, star, style_frame)
+        time.sleep(delay_frame)
+        canvas.refresh()
 
 
 if __name__ == '__main__':
-
-    curses.update_lines_cols()
-    curses.wrapper(draw)
+    while True:
+        curses.update_lines_cols()
+        curses.wrapper(draw)
