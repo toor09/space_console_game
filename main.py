f8c338272e94445382ae316db5f7495d9c319522
Danil Nikitin <d.nikitin.dev@gmail.com>
2022-08-11 15:58:12 +0300

    Added a coroutine to infinitely animate a star.

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
+import asyncio
 import curses
 import time
-from collections import namedtuple
+
+
+async def blink(canvas, row, column, symbol='*'):  # type: ignore
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):  # type: ignore
     row, column = (5, 20)
     canvas.border()
     curses.curs_set(False)
-    star = "*"
-    StarFrame = namedtuple("StarFrame", "delay style")
-    dim_star_frame = StarFrame(delay=2, style=curses.A_DIM)
-    bold_star_frame = StarFrame(delay=0.3, style=curses.A_BOLD)
-    default_star_frame = StarFrame(delay=0.3, style=0)
-    frames = (
-        dim_star_frame,
-        default_star_frame,
-        bold_star_frame,
-        default_star_frame,
-    )
-    for delay_frame, style_frame in frames:
-        canvas.addstr(row, column, star, style_frame)
-        time.sleep(delay_frame)
-        canvas.refresh()
+    coroutine_blink = blink(canvas, row, column)  # type: ignore
+    while True:
+        try:
+            coroutine_blink.send(None)
+            canvas.refresh()
+            time.sleep(1)
+        except StopIteration:
+            break
 
 
 if __name__ == '__main__':
-    while True:
-        curses.update_lines_cols()
-        curses.wrapper(draw)
+    curses.update_lines_cols()
+    curses.wrapper(draw)
